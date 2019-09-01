59fd1ba7a0fad888f54a06b61210b4aa9f6b1345
hadibastanfar <98bastanfar@gmail.com>
2019-09-02 01:20:06 +0430

    Added left margin to list item text. Removed dismissible direction.

--- lib/widgets/list_item.dart
+++ lib/widgets/list_item.dart
@@ -12,51 +12,50 @@ class ListItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final DatabaseHelper _dataBaseHelper = DatabaseHelper.instance;
-    final double _height = MediaQuery
-        .of(context)
-        .size
-        .height;
+    final double _height = MediaQuery.of(context).size.height;
     final double _itemHeight = _height * .1;
     return ScopedModelDescendant<Scope>(
-      builder: (BuildContext context, Widget child, Scope model) =>
-          Dismissible(
-            key: Key("${item.id}"),
-            direction: DismissDirection.startToEnd,
-            onDismissed: (direction) {
-              model.delete(item.id);
-            },
-            child: Container(
-              margin: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
-              height: _itemHeight,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.all(Radius.circular(10)),
-              ),
-              child: Material(
-                color: Colors.white,
-                borderRadius: BorderRadius.all(Radius.circular(10)),
-                elevation: 5,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: <Widget>[
-                    Text("${item.todoDescription}"),
-                    Expanded(child: SizedBox()),
-                    Checkbox(
-                      value: item.isDone,
-                      onChanged: (bool value) {
-                        final map = {
-                          DatabaseHelper.columnTodoDescription: item.todoDescription,
-                          DatabaseHelper.columnIsDone: !item.isDone,
-                          DatabaseHelper.columnId: item.id,
-                        };
-                        model.update(map);
-                      },
-                    ),
-                  ],
+      builder: (BuildContext context, Widget child, Scope model) => Dismissible(
+        key: Key("${item.id}"),
+        onDismissed: (direction) {
+          model.delete(item.id);
+        },
+        child: Container(
+          margin: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+          height: _itemHeight,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.all(Radius.circular(10)),
+          ),
+          child: Material(
+            color: Colors.white,
+            borderRadius: BorderRadius.all(Radius.circular(10)),
+            elevation: 5,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.only(left: 15),
+                  child: Text("${item.todoDescription}"),
                 ),
-              ),
+                Expanded(child: SizedBox()),
+                Checkbox(
+                  value: item.isDone,
+                  onChanged: (bool value) {
+                    final map = {
+                      DatabaseHelper.columnTodoDescription:
+                          item.todoDescription,
+                      DatabaseHelper.columnIsDone: !item.isDone,
+                      DatabaseHelper.columnId: item.id,
+                    };
+                    model.update(map);
+                  },
+                ),
+              ],
             ),
           ),
+        ),
+      ),
     );
   }
 }
