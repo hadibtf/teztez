9166d4b1fbd5083b484aeb2f70cf09c175aa25bf
hadibastanfar <98bastanfar@gmail.com>
2019-09-03 01:24:20 +0430

    Added drawer(not yet fonctional). Added custom dialog to add custom description for todo. Added Toast to project. Some code clean up on Scope class.

--- lib/pages/main.dart
+++ lib/pages/main.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:scoped_model/scoped_model.dart';
 import 'package:teztez_app/scp_model/scope.dart';
+import 'package:toast/toast.dart';
 import '../widgets/list_item.dart';
-import '../models/todo_model.dart';
-import '../database/database_helper.dart';
 
 class MainPage extends StatelessWidget {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  String _description;
+
   @override
   Widget build(BuildContext context) {
     return ScopedModelDescendant<Scope>(
       builder: (BuildContext context, Widget child, Scope model) {
         return Scaffold(
+          drawer: _buildDrawer(context),
 //      backgroundColor: Colors.grey,
           appBar: AppBar(title: Text("Todo")),
           body: ListView.builder(
@@ -20,13 +23,120 @@ class MainPage extends StatelessWidget {
             },
           ),
           floatingActionButton: FloatingActionButton(
+            child: Icon(Icons.add),
             onPressed: () {
-              model.insert();
-              model.updateTodoList();
+              showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return Dialog(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(25),
+                    ),
+                    elevation: 0,
+                    child: Container(
+                      child: Padding(
+                        padding: EdgeInsets.all(15),
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: <Widget>[
+                            Form(
+                              key: _formKey,
+                              child: TextFormField(
+                                initialValue: "",
+                                cursorColor: Colors.black,
+                                decoration: InputDecoration(
+                                    hintText: "Type your ToDo here"),
+                                onSaved: (String value) {
+                                  _description = value;
+                                },
+                                validator: (String value) {
+                                  String errorMsg;
+                                  if (value.isEmpty) errorMsg = "Input can't be empty!";
+                                  return errorMsg;
+                                },
+                              ),
+                            ),
+                            SizedBox(height: 25),
+                            SizedBox(
+                              height: 50,
+                              child: Row(
+                                children: <Widget>[
+                                  Expanded(
+                                    child: Center(
+                                      child: FlatButton(
+                                        textColor: Colors.blue,
+                                        child: Text("Add"),
+                                        onPressed: () =>
+                                            addToList(model, context),
+                                      ),
+                                    ),
+                                  ),
+                                  Expanded(
+                                    child: Center(
+                                      child: FlatButton(
+                                        textColor: Colors.black,
+                                        child: Text("Close"),
+                                        onPressed: () {
+                                          Navigator.of(context).pop();
+                                        },
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(25),
+                      ),
+                    ),
+                  );
+                },
+              );
             },
           ),
         );
       },
+    );
+  }
+
+  void addToList(Scope model, BuildContext context) {
+    if (!_formKey.currentState.validate()) return;
+    _formKey.currentState.save();
+    model.insert(_description);
+    Toast.show("Added", context);
+    Navigator.of(context).pop();
+  }
+
+  Widget _buildDrawer(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: <Widget>[
+          AppBar(
+            automaticallyImplyLeading: false,
+            title: Text("Menu"),
+          ),
+          ListTile(
+            leading: Icon(
+              Icons.done,
+              color: Colors.green,
+            ),
+            title: Text("Done"),
+            onTap: () {
+              Toast.show("Hello world", context, duration: Toast.LENGTH_LONG);
+            },
+          ),
+          Divider(),
+          ListTile(
+            leading: Icon(Icons.list),
+            title: Text('Todo List'),
+            onTap: () {},
+          )
+        ],
+      ),
     );
   }
 }
--- lib/scp_model/scope.dart
+++ lib/scp_model/scope.dart
@@ -1,20 +1,18 @@
 import 'package:scoped_model/scoped_model.dart';
-//import 'dart:async';
 
 import '../database/database_helper.dart';
 import '../models/todo_model.dart';
 
 class Scope extends Model {
   final DatabaseHelper _dataBaseHelper = DatabaseHelper.instance;
-  List<Todo> _todos = [];
+  List<Todo> _todoList = [];
 
-  List<Todo> get todoList {
-    return List.of(_todos);
-  }
+  List<Todo> get todoList => List.of(_todoList);
 
   void updateTodoList() async {
-    final List<Map<String, dynamic>> allRows = await _dataBaseHelper.queryAllRows();
-    _todos = List.generate(
+    final List<Map<String, dynamic>> allRows =
+        await _dataBaseHelper.queryAllRows();
+    _todoList = List.generate(
       allRows.length,
       (i) => Todo(
         allRows[i][DatabaseHelper.columnId],
@@ -25,37 +23,22 @@ class Scope extends Model {
     notifyListeners();
   }
 
-  void insert() async {
-    // row to insert
+  void insert(String description) {
     Map<String, dynamic> row = {
       DatabaseHelper.columnIsDone: 0,
-      DatabaseHelper.columnTodoDescription: "Hello, World!"
+      DatabaseHelper.columnTodoDescription: description
     };
-    final id = await _dataBaseHelper.insert(row);
-    print('inserted row id: $id');
+    _dataBaseHelper.insert(row);
     updateTodoList();
   }
 
-  void query() async {
-    final allRows = await _dataBaseHelper.queryAllRows();
-    print('query all rows:');
-    allRows.forEach((row) {
-      print(row);
-    });
-    notifyListeners();
-  }
-
-  void update(Map<String, dynamic> map) async {
-    final rowsAffected = await _dataBaseHelper.update(map);
-    print('updated $rowsAffected row(s)');
+  void update(Map<String, dynamic> map) {
+    _dataBaseHelper.update(map);
     updateTodoList();
   }
 
-  void delete(int id) async {
-    // Assuming that the number of rows is the id for the last row.
-//    final id = await _dataBaseHelper.queryRowCount();
-    final rowsDeleted = await _dataBaseHelper.delete(id);
-    print('deleted $rowsDeleted row(s): row $id');
+  void delete(int id) {
+    _dataBaseHelper.delete(id);
     updateTodoList();
   }
 }
